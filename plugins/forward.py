3846d619262865f6be2065623b3efd3e7a4eb742
SBZGDrive <119879186+SBZGDrive@users.noreply.github.com>
2023-02-27 18:26:40 +0530

    Update forward.py

--- plugins/forward.py
+++ plugins/forward.py
@@ -11,11 +11,10 @@ import os
 @don.on_message(filters.private & filters.command("shivam"))
 async def forward(bot: don , m: Message):
     msg = await bot.send_message(m.from_user.id, "**Forward any message from the Target channel\nBot should be admin at both the Channels**")
-    t_chat = msg.forward_from.chat.id
-#            msg.forward_from_chat.id
+    #t_chat = msg.forward_from.chat.id
     msg1 = await bot.send_message(m.chat.id, "**Send Starting Message From Where you want to Start forwarding**")
     msg2 = await bot.send_message(m.chat.id, "**Send Ending Message from same chat**")
-   # print(msg1.forward_from_message_id, msg1.forward_from_chat.id, msg1.forward_from_message.id)
+    # print(msg1.forward_from_message_id, msg1.forward_from_chat.id, msg1.forward_from_message.id)
     i_chat = msg1.forward_from.chat.id
     s_msg = int(msg1.forward_from.chat.id)
     f_msg = int(msg2.forward_from.chat.id)+1
